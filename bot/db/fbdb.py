94f4a7cff974dc139e2e9c2b8d9c3d7716edd9be
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2023-03-07 10:40:30 -0800

    fbdb

--- bot/db/fbdb.py
+++ bot/db/fbdb.py
@@ -5,11 +5,11 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
+print("fbdb.py init firestore")
 project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
 print(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
 # If you have a cred dict set in the env
-# cred = credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS) # UNFORTUNATLEY CAN'T USE THIS BECUASE  GOOGLE_APPLICATION_CREDENTIALS HAS TO BE SET IN ENV FOR DIALOGFLOW
+# cred = credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS) # UNFORTUNATLEY CAN'T USE THIS BECUASE GOOGLE_APPLICATION_CREDENTIALS HAS TO BE SET IN ENV FOR DIALOGFLOW
 # Use the application default credentials
 cred = credentials.ApplicationDefault() # If you have a credfile set in the env
 
