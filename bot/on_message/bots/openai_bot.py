93f22859ab00760fa97df952af8b51e089da0126
Hasan Haq <hasan.haq@gmail.com>
2024-11-12 01:17:04 -0600

    gotta add new content here

--- bot/on_message/bots/openai_bot.py
+++ bot/on_message/bots/openai_bot.py
@@ -195,7 +195,7 @@ class OpenAIBot:
 
         response_text = self._get_response_or_weezerpedia_function_call_results(new_content, True)
         function_call_content =  [{"role": "user", "content": f"Incorporate the following Weezerpedia entry into your response, to the extent it is relevant. \n {response_text}"}]
-        response_text = self._get_response_or_weezerpedia_function_call_results(function_call_content, False)
+        response_text = self._get_response_or_weezerpedia_function_call_results(new_content + function_call_content, False)
         new_content.append(
             {"role": "assistant", "content": response_text}
         )
